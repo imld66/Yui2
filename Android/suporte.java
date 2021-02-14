84fdaaa9225d65bdac0bac85f17794d8fe5a3270
imld66 <cesarjuliotp@gmail.com>
2021-02-14 19:42:26 -0300

    First commit

---
+++ Android/suporte.java
@@ -0,0 +1,9 @@
+/*
+ * Classname             (yui 2.0)
+ *
+ * Version information   (2.0)
+ *
+ * Date                  (06/12,15:35)
+ *
+ * author                (ImLD)
+ * Copyright notice      (bot com suporte a Android/Java)
---
+++ src/help.js
@@ -0,0 +1,171 @@
+const //database
+	return `
+╔══✪〘 INFO 〙✪══
+║
+╠➥ Yui Bot
+╠➥ *2.0*
+╠➥ 𝐃𝐎𝐍𝐎:  ImLD
+╠➥ *wa.me/+5547999798506*
+╠➥ 𝐒𝐓𝐀𝐓𝐔𝐒: ON
+║
+╠══✪〘 NOVIDADES 〙✪══
+║
+║1 *${prefix}qrcode [premium]*
+║2 *${prefix}gcpf [premium]*
+║3 *${prefix}gbin [premium]*
+║4 *${prefix}destrava [premium]*
+║5 *${prefix}gpessoa [premium]*
+║6 *${prefix}wame*
+║7 *${prefix}spamcall*
+║8 *${prefix}play (nome da msc)*
+║
+╠══✪〘 MENU 〙✪══
+║
+║9 *${prefix}figu*
+║10 *${prefix}toimg*
+║11 *${prefix}meme*
+║12 *${prefix}memeindo*
+║13 *${prefix}tts*
+║14 *${prefix}lolih [on]*
+║15 *${prefix}nsfwloli [off]*
+║16 *${prefix}url2img*
+║17 *${prefix}leens [na legenda]*
+║18 *${prefix}wait [na legenda]*
+║19 *${prefix}setprefix*
+║
+╠══✪〘 OUTROS 〙✪══
+║
+║20 *${prefix}linkgp*
+║21 *${prefix}simih [1/0]*
+║22 *${prefix}marcar*
+║23 *${prefix}add [@]*
+║24 *${prefix}banir [@]*
+║25 *${prefix}promover [@]*
+║26 *${prefix}rebaixar*
+║27 *${prefix}admins*
+║28 *${prefix}marcar2*
+║29 *${prefix}bc [texto]* (ele faz uma ™)
+║30 *${prefix}marcar3*
+║31 *${prefix}bloqueados*
+║32 *${prefix}bloquear [@]*
+║33 *${prefix}desbloquear [@]*
+║34 *${prefix}limpar*
+║35 *${prefix}bc [ *texto* ]*
+║36 *${prefix}bemvindo [1/0]*
+║37 *${prefix}clonar [@]*
+║38 *${prefix}help1*
+║39 *${prefix}dono*
+║40 *${prefix}owner*
+║41 *${prefix}tts [texto]*
+║42 *${prefix}setnome*
+║43 *${prefix}termux*
+║44 *${prefix}setfoto*
+║45 *${prefix}grupoinfo*
+║46 *${prefix}ytmp4*
+║47 *${prefix}bomdia*
+║48 *${prefix}boanoite*
+║49 *${prefix}marcar*
+║50 *${prefix}marcar2*
+║51 *${prefix}marcar3*
+║
+╠══✪〘 IMAGENS 〙✪══
+║
+║52 *${prefix}dono*
+║53 *${prefix}boanoite*
+║54 *${prefix}bomdia*
+║55 *${prefix}boatarde*
+║56 *${prefix}meme*   
+║57 *${prefix}lofi*
+║58 *${prefix}reislin*
+║
+╠══✪〘 INTELIGÊNCIA IA 〙✪══
+║
+║59 *${prefix}simih 1 (para ativar)*
+║60 *${prefix}simih 0 (para desativar)*
+║ *${prefix}simi (sua mensagem)*
+║
+╠══✪〘 EM TESTE 〙✪══
+║
+║61 *${prefix}*
+║62 *${prefix}*
+║63 *${prefix}*
+║
+╠══✪〘 PREMIUM 〙✪══
+║
+║64 *${prefix}dado*
+║65 *${prefix}cekvip*
+║66 *${prefix}premiumlist*
+║67 *${prefix}delete*
+║68 *${prefix}modapk*
+║69 *${prefix}indo10*
+║70 *${prefix}daftarvip [para virar Premium]*
+║71 *${prefix}qrcode*
+║72 *${prefix}gcpf*
+║73 *${prefix}gbin*
+║74 *${prefix}destrava*
+║75 *${prefix}gpessoa*
+║
+╠══✪〘 GRUPO 〙✪══
+║
+║76 *${prefix}banir*
+║77 *${prefix}leveling [on/off]*
+║78 *${prefix}level*
+║79 *${prefix}add*
+║80 *${prefix}promover*
+║81 *${prefix}setfoto [na legenda]*
+║82 *${prefix}setname [texto]*
+║83 *${prefix}rebaixar*
+║84 *${prefix}admins*
+║85 *${prefix}marcar*
+║86 *${prefix}marcar2*
+║87 *${prefix}marcar3*
+║88 *${prefix}bemvindo [1/0]*
+║89 *${prefix}grupoinfo*
+║90 *${prefix}bomdia*
+║91 *${prefix}boatarde*
+║92 *${prefix}boanoite*
+║93 *${prefix}setdesc*
+║94 *${prefix}bug [sua mensagem]*
+║
+╠══✪〘 ESPECIFICO DO BOT 〙✪══
+║
+║95 *${prefix}bug [sua mensagem]*
+║96 *${prefix}clonar [@]*
+║97 *${prefix}dono*
+║98 *${prefix}ping [ver velocidade do bot]*
+║99 *${prefix}termux*
+║100 *${prefix}gay [@]*
+║101 *${prefix}wame*
+║102 *${prefix}map (nome)*
+║103 *${prefix}setppbot (marque uma img)*
+║103 *${prefix}pinterest (nome)*
+║105 *${prefix}desligar (so para o dono)*
+║106 *${prefix}timer*
+║
+╠══✪〘 MAIS ALGUNS 〙✪══
+║
+║107 *${prefix}neko*
+║108 *${prefix}ttp [texto]*
+║109 *${prefix}testime*
+║110 *${prefix}tomp3*
+║111 *${prefix}animehug*
+║112 *${prefix}shoturl*
+║112 *${prefix}pokemon*
+║
+║
+╠══✪〘 𝗗𝗢𝗡𝗢 〙✪══
+║
+║ *NOME: ImLD e Storm*
+║ *INSTA: taiyo.gg*
+║ *WPP: wa.me/+5547999798506*
+║
+║
+║  *"Yui"*
+║
+║
+╚═〘 Yui Bot 〙`
+}
+
+//Online.database
+
+
